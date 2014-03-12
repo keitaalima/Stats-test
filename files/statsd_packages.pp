54ce2f4a656b257c62730af24480f34f3264f522
Alimatou Keita <keitaalm@aol.com>
2014-03-12 17:56:11 -0400

    Added the statsd package file

---
+++ files/statsd_packages.pp
@@ -0,0 +1,15 @@
+#statsd package installation
+
+class statsd-package{
+	
+	exec { 'install_node':
+		command => '/opt/ git clone https://github.com/joyent/node 
+		creates => '/opt/node'
+ 
+	}
+
+	exec {'install_npm':
+		command => '/tmp/ wget --no-check-certificate http://npmjs.org/install.sh | sh install.sh'
+		creates => '/tmp/install.sh'
+	}
+}
