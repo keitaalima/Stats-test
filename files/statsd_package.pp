b8c0b01de2ec17e8470d2420b1652fc8fec49ebe
Alimatou Keita <keitaalm@aol.com>
2014-03-13 14:30:15 -0400

    Changes

--- files/statsd_package.pp
+++ files/statsd_package.pp
@@ -1,16 +1,49 @@
 #statsd package installation
 
 class statsd-package{
+
+	package {'node': ensure => present}
+	package {'npm': ensure => present}
+
 	
-	exec { 'install_node':
+	exec {'install_node':
 		command => '/opt/ git clone https://github.com/joyent/node 
 		creates => '/opt/node'
- 
 	}
 
-	exec {'install_npm':
-		command => '/tmp/ wget --no-check-certificate http://npmjs.org/install.sh | sh install.sh'
-		creates => '/tmp/install.sh'
+	file {'/opt/node/':
+		ensure => directory	
+	}
+
+	exec {'install_node':
+		command => '/opt/node ; ./configure && make && make install'
+		require => Package ['nodejs'],
+	}
+
+	file {'/usr/local/bin/node':
+		ensure => present
+	}
+
+## installing npm in the /tmp directory
+
+#	package {'install.sh':
+#		name = $install.sh
+#		ensure => latest,
+#		source => '/tmp/$install.sh'
+##		provider => 'wget'
+#	}
+
+#	exec {'/tmp/ wget --no-check-certificate http://npmjs.org/install.sh ; sh install.sh':
+#		creates => '/tmp/install.sh'
+#		subscribe => Package ['install.sh']
+#	}
+
+	file {'/usr/bin/npm':
+		ensure => present 
+	}
+
+	exec {'/usr/bin/npm install express':
+		require => Package ['npm']
 	}
 }
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,60 @@
-class statsd{
-	
+class statsd ($graphite_host, $graphite_port = 2003, $port = 8125, $debug = true, {
 
+## the statsd package 
+
+	package {'statsd':
+		ensure => present
+	}	
+
+	file {'/opt/statsd':
+    	ensure => directory,
+    	owner => 'root',
+    	group => 'root',
+    	mode => '0755',
+	}
 	
+#init script for statsd
+
+	file { '/etc/init.d/statsd':
+		ensure => file,
+		owner => root, 
+		mode => '0644',
+		group => 'root,
+		source => 'puppet:///modules/statsd/statsd-init.sh',
+		notify => Service['statsd'],
+	}
+
+#configuration files that need to be in the opt/statsd directory
+	file { '/opt/statsd/local.js':
+		ensure => file,
+		source => 'puppet:///modules/statsd/local.js',
+		mode  => '0755',
+	}
+
+	file {'/opt/statsd/statsdconf.js':
+		ensure => file,
+		owner => root,
+		group => root,
+		mode  => '0775'
+		content => template('statsd/statsdconf.js.erb')
+		notify => Service['statsd'],
+	
+	}
+
+#ping file for testing purposes, to make sure connection is established between statsd and graphite
+ 	
+ 	file {'/opt/statsd/stats_test.py':
+ 		ensure => file,
+ 		source => 'puppet:///modules/statsd/stats_test.py'
+ 		mode => '0755',
+ 	} 
+	
+	service {'statsd':
+		ensure     => running,
+   	 	enable     => true,
+    	hasrestart => true,
+    	hasstatus  => true,
+    	require    => Package['statsd'],
+	}
+
 }
